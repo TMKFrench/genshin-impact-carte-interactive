a6703d75d409096a8270b04dbdfb4d373c8affd5
Anthony Destenay <anthony.destenay@gmail.com>
2021-06-09 11:04:28 +0200

    Ajout de l'archipel de la pomme dorée

---
+++ assets/js/script-archipel-pomme-doree.js
@@ -0,0 +1,639 @@
+function onMapClick(e) {
+    console.log("Position @ " + map.project([e.latlng.lat, e.latlng.lng], map.getMaxZoom()));
+    console.log("Point @ [" + e.latlng.lat + ", " + e.latlng.lng + "]");
+  }
+
+  function unproject(coord) {
+    return map.unproject(coord, map.getMaxZoom());
+  }
+
+  function updateCurrentMarker() {
+    currentMarker = this;
+  }
+
+  function updateUserCountdowns(checked) {
+    var countdowns = getUserCountdowns();
+
+    if(typeof(currentMarker._tooltip) !== 'undefined' && currentMarker._tooltip.options.className === 'countdown') {
+      var cid = currentMarker._tooltip.options.countdownid;
+      if(checked) {
+        var nextTime = moment();
+        nextTime.add(currentMarker._tooltip.options.countdown, 'hours');
+        currentMarker._tooltip.setContent('<div id="countdown-'+currentMarker._tooltip.options.countdownid+'">Prochain reset :</div>');
+
+        // var nextTime = moment().add(10, 'seconds');
+        $($('#countdown-'+currentMarker._tooltip.options.countdownid)).countdown(nextTime.toDate(), function(event) {
+
+          if(event.type === 'finish') {
+            resetCountdown(cid);
+            $(this).html('');
+          } else {
+            var totalDays = event.offset.weeks * 7 + event.offset.days;
+            if(totalDays > 0) {
+              $(this).html('Prochain reset :<br />'+event.strftime('%-D j. %H:%M:%S'));
+            } else {
+              $(this).html('Prochain reset :<br />'+event.strftime('%H:%M:%S'));
+            }
+          }
+
+        });
+
+        if(searchId(cid, countdowns) <= 0) {
+          countdowns.push({id: cid, date: nextTime.format('YYYY-MM-DD HH:mm:ss')});
+        }
+
+      } else {
+        var i = searchId(cid, countdowns);
+        if(i >= 0) {
+          countdowns.splice(i, 1);
+        }
+
+        currentMarker._tooltip.setContent('<div id="countdown-'+currentMarker._tooltip.options.countdownid+'"></div>');
+
+      }
+
+      localStorage.setItem('userCountdowns', JSON.stringify(countdowns));
+      userCountdowns = JSON.stringify(countdowns);
+    }
+  }
+
+  function saveUserMarker(uid, checked) {
+    var markers = getUserMarkers();
+
+    updateUserCountdowns(checked);
+
+    if(checked) {
+      $.post('api/addmarker/'+uid, function(res) {
+        if(typeof(res.error) !== 'undefined') {
+          alert('Vous avez été déconnecté. La page va se rafraîchir.');
+          window.location.reload();
+        }
+
+        currentMarker.setOpacity(.5);
+        userMarkers = res.markers;
+      });
+    } else {
+      $.post('api/removemarker/'+uid, function(res) {
+        if(typeof(res.error) !== 'undefined') {
+          alert('Vous avez été déconnecté. La page va se rafraîchir.');
+          window.location.reload();
+        }
+
+        currentMarker.setOpacity(1);
+        userMarkers = res.markers;
+      });
+    }
+  }
+
+  function searchId(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+      if (myArray[i].id === nameKey) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  function resetCountdown(cid) {
+    var markers = getUserMarkers();
+    var countdowns = getUserCountdowns();
+    var i = searchId(cid, countdowns);
+    if(i >= 0) {
+      allMarkers[cid].setOpacity(1);
+      countdowns.splice(i, 1);
+    }
+    if(markers.indexOf(cid) >= 0) {
+      markers.splice(markers.indexOf(cid), 1);
+    }
+    localStorage.setItem('userCountdowns', JSON.stringify(countdowns));
+    userCountdowns = JSON.stringify(countdowns);
+    localStorage.setItem('userMarkers', JSON.stringify(markers));
+    userMarkers = JSON.stringify(markers);
+  }
+
+  function saveUserMarkers(uid, checked) {
+    var markers = getUserMarkers();
+
+    updateUserCountdowns(checked);
+
+    if(checked) {
+      if(markers.indexOf(uid) < 0) {
+        markers.push(uid);
+      }
+      currentMarker.setOpacity(.5);
+    } else {
+      if(markers.indexOf(uid) >= 0) {
+        markers.splice(markers.indexOf(uid), 1);
+      }
+      currentMarker.setOpacity(1);
+    }
+
+    localStorage.setItem('userMarkers', JSON.stringify(markers));
+    userMarkers = JSON.stringify(markers);
+  }
+
+  function getUserCountdowns() {
+    var countdowns = localStorage.getItem('userCountdowns');
+
+    if(!countdowns) {
+      countdowns = [];
+    } else {
+      countdowns = JSON.parse(countdowns);
+    }
+
+    return countdowns;
+  }
+
+  function getUserMarkers() {
+    var markers = localStorage.getItem('userMarkers');
+
+    if(!markers) {
+      markers = [];
+    } else {
+      markers = JSON.parse(markers);
+    }
+
+    return markers;
+  }
+
+  function getParamsURL() {
+    var url = location.search,
+        query = url.substr(1),
+        result = {};
+
+    query.split("&").forEach(function(part) {
+      var item = part.split("=");
+      result[item[0]] = decodeURIComponent(item[1]);
+    });
+
+    return result;
+  }
+
+  function popUpOpen(e) {
+    var content = e.popup.getContent();
+
+    if($(content).find('input#user-marker').length > 0) {
+      // var userMarkers = getUserMarkers();
+      if(userMarkers.indexOf( $(content).find('input#user-marker').first().data('id') ) >= 0) {
+        $('input#user-marker[data-id="'+$(content).find('input#user-marker').first().data('id')+'"]').prop('checked', 'checked');
+      }
+    }
+  }
+
+
+
+  var currentMarker;
+  var total = 0;
+  var params = getParamsURL();
+  var userMarkers = getUserMarkers();
+  var userCountdowns = getUserCountdowns();
+  var debugMarkers = [];
+  var userLocal = true;
+  var allMarkers = [];
+
+
+
+  // Liste des icons
+  var teleporterIcon = L.icon({ iconUrl: 'assets/img/teleporter.png', iconSize: [32,32], iconAnchor: [16,32], popupAnchor: [0, -32] });
+  var echoshellIcon = L.icon({ iconUrl: 'assets/img/echoshell.png', iconSize: [30,30], iconAnchor: [15,15], popupAnchor: [0,-15] });
+  var waveriderIcon = L.icon({ iconUrl: 'assets/img/waverider.png', iconSize: [32,32], iconAnchor: [16,16], popupAnchor: [0,-16] });
+
+
+
+  // Initialisation de la carte
+  var map = new L.Map('map', {
+      center : [0,0],
+      zoom : 4,
+      zoomControl: false
+  });
+
+
+
+  // Générer les layers
+  var groups = [
+    'teleporter', 'echoshell', 'waverider'
+  ];
+  groups.forEach(function(e) {
+    window[e+'Group'] = L.layerGroup();
+  });
+
+
+
+  // Liste des marqueurs
+  var markers = [
+    {
+      id: 'echoshell',
+      group: echoshellGroup,
+      icon: echoshellIcon,
+      format: 'image',
+      checkbox: true,
+      markers: [
+        {
+          id: 'I3',
+          title: 'Échos du passé - I<br />Une rivière coule au milieu',
+          coords: [4818, 3476]
+        },
+        {
+          id: 'I4',
+          title: 'Échos du passé - I<br />Quatre vents font résonner la joie',
+          coords: [4906, 3237]
+        },
+        {
+          id: 'IV1',
+          title: 'Échos du passé - IV<br />Le général',
+          coords: [5026, 4626]
+        },
+        {
+          id: 'VII6',
+          title: 'Échos du passé - VII<br />Le prix d\'avoir essayé',
+          coords: [5100, 4584]
+        },
+        {
+          id: 'VII7',
+          title: 'Échos du passé - VII<br />Sans retour',
+          coords: [3814, 3588]
+        },
+      ]
+    },
+    {
+      id: 'waverider',
+      group: waveriderGroup,
+      icon: waveriderIcon,
+      markers: [
+        {
+          id: '01',
+          coords: [5024, 4614]
+        },
+        {
+          id: '02',
+          coords: [4744, 3380]
+        },
+        {
+          id: '03',
+          coords: [3595, 3750]
+        },
+        {
+          id: '04',
+          coords: [4103, 5074]
+        },
+      ]
+    },
+    {
+      id: 'teleporter',
+      group: teleporterGroup,
+      icon: teleporterIcon,
+      format: 'image',
+      checkbox: true,
+      markers: [
+        {
+          id: 'archipel01',
+          coords: [5067, 4607],
+        },
+        {
+          id: 'archipel02',
+          coords: [4859, 3218],
+        },
+        {
+          id: 'archipel03',
+          coords: [3620, 3788],
+        },
+        {
+          id: 'archipel04',
+          coords: [4046, 5030],
+        },
+      ]
+    },
+  ];
+
+
+  // Création de la carte
+  L.tileLayer('assets/img/tiles-archipel-pomme-doree/{z}/{x}/{y}.jpg', {
+    attribution: '<a href="https://gaming.lebusmagique.fr">Le Bus Magique Gaming</a>',
+    maxZoom: 4,
+    minZoom: 3,
+    continuousWorld: true,
+    maxBoundsViscosity: 0.8,
+    noWrap: true
+  }).addTo(map);
+
+  var toolbarZoom = L.easyBar([
+    L.easyButton( '<img src="assets/img/plus.png" alt="Zoom+" title="Zoomer sur la carte">',  function(control, map){map.setZoom(map.getZoom()+1);}),
+    L.easyButton( '<img src="assets/img/minus.png" alt="Zoom-" title="Dézoomer sur la carte">',  function(control, map){map.setZoom(map.getZoom()-1);}),
+  ]);
+
+  var toolbarMenu = L.easyBar([
+    L.easyButton( '<img src="assets/img/menu.png" alt="Menu" title="Afficher/Masquer le menu">',  function(control, map){
+      $('body').toggleClass('show-menu');
+      map.invalidateSize();
+    }),
+  ]);
+
+  var toolbarRegion = L.easyBar([
+    L.easyButton( '<img src="assets/img/region.png" alt="Region" title="Afficher/Masquer le nom des régions">',  function(control, map){
+      if(map.hasLayer(regionGroup)) {
+        map.removeLayer(regionGroup);
+      } else {
+        map.addLayer(regionGroup);
+      }
+    }),
+  ]);
+
+  var toolbarResetMarkers = L.easyBar([
+    L.easyButton( '<img src="assets/img/reset.png" alt="Réinitialiser" title="Réinitialiser le suivi de vos marqueurs">',  function(control, map){
+      if(confirm('Êtes-vous sûr de vouloir supprimer le suivi de vos marqueurs ?')) {
+        if(userLocal) {
+          localStorage.removeItem('userMarkers');
+          localStorage.removeItem('userCountdowns');
+          window.location.reload();
+        } else {
+          $.post('api/resetmarkers', function(res) {
+            if(typeof(res.error) !== 'undefined') {
+              alert('Vous avez été déconnecté. La page va se rafraîchir.');
+            }
+            localStorage.removeItem('userMarkers');
+            localStorage.removeItem('userCountdowns');
+            window.location.reload();
+          });
+        }
+      }
+    }),
+  ]);
+
+  var toolbarInfo = L.easyBar([
+    L.easyButton( '<img src="assets/img/info.png">',  function(control, map){var lightbox = lity('#info');}),
+  ]);
+
+  toolbarZoom.addTo(map);
+  toolbarMenu.addTo(map);
+  toolbarRegion.addTo(map);
+  toolbarResetMarkers.addTo(map);
+  toolbarInfo.addTo(map);
+
+
+
+  // Génération des marqueurs
+  function initMarkers() {
+    markers.forEach(function(g) {
+
+      g.markers.forEach(function(m){
+        var checkbox = '', icon, format, title = '', text = '', guide = '', countdown, timer, finished = false, color = '#3388ff';
+
+        if((typeof m.checkbox !== 'undefined' && m.checkbox) || (typeof g.checkbox !== 'undefined' && g.checkbox))
+          checkbox = '<label><input type="checkbox" id="user-marker" data-id="'+g.id+m.id+'" /><span>Terminé</span></label>';
+
+        if(typeof g.text !== 'undefined')
+          text = '<p>'+g.text+'</p>';
+        if(typeof m.text !== 'undefined')
+          text = '<p>'+m.text+'</p>';
+
+        if(typeof g.title !== 'undefined')
+          title = '<h4>'+g.title+'</h4>';
+        if(typeof m.title !== 'undefined')
+          title = '<h4>'+m.title+'</h4>';
+
+        if(typeof g.guide !== 'undefined')
+          guide = '<a href="'+g.guide+'" class="guide" target="_blank">Guide</a>';
+        if(typeof m.guide !== 'undefined')
+          if(typeof g.guide !== 'undefined' && m.guide.substr(0, 1) === '#')
+            guide = '<a href="'+g.guide+m.guide+'" class="guide" target="_blank">Guide</a>';
+          else
+            guide = '<a href="'+m.guide+'" class="guide" target="_blank">Guide</a>';
+
+        icon = (typeof m.icon !== 'undefined') ? m.icon : g.icon;
+        format = (typeof m.format !== 'undefined') ? m.format : g.format;
+
+        if(typeof g.countdown !== 'undefined')
+          countdown = g.countdown;
+        if(typeof m.countdown !== 'undefined')
+          countdown = m.countdown;
+
+        if(typeof g.timer !== 'undefined')
+          timer = g.timer;
+        if(typeof m.timer !== 'undefined')
+          timer = m.timer;
+
+        if(typeof g.color !== 'undefined')
+          color = g.color;
+        if(typeof m.color !== 'undefined')
+          color = m.color;
+
+        var marker = L.marker(unproject(m.coords), {icon: icon});
+
+        if(format === 'popup')
+          marker.bindPopup(title+text+guide+checkbox);
+        else if(format === 'video')
+          marker.bindPopup(title+'<a class="video" href="//www.youtube.com/watch?v='+m.video+'" data-lity><img src="https://i.ytimg.com/vi/'+m.video+'/hqdefault.jpg" /></a>'+text+guide+checkbox);
+        else if(format === 'image')
+          marker.bindPopup(title+'<a href="assets/img/medias/'+g.id+m.id+'.jpg" class="image" data-lity><img src="thumb/'+g.id+m.id+'" /></a>'+text+guide+checkbox);
+        else if(format === 'banner')
+          marker.bindPopup(title+'<img src="assets/img/medias/'+g.id+m.id+'.jpg" onerror="this.src=\'assets/img/medias/default.jpg\'" />'+text+guide+checkbox);
+        else if(format === 'region')
+          marker.bindTooltip(m.title, {permanent: true, className: 'region', offset: [0, 13], direction: 'top'}).openTooltip();
+
+        if(typeof(timer) !== 'undefined') {
+          var nextTime = moment();
+          if(nextTime.get('hour') >= 4) {
+            nextTime.add(1, 'day');
+          }
+          nextTime.set({hour: 4, minute: 0, second: 0, millisecond: 0});
+          marker.bindTooltip('<div id="timer-'+g.id+m.id+'">Prochain reset :</div>', {timerid: g.id+m.id, timer: nextTime.toDate(), permanent: true, className: 'timer', offset: [0, 0], direction: 'bottom'}).openTooltip();
+          marker.on('tooltipopen', function(e) {
+            $($('#timer-'+e.tooltip.options.timerid)).countdown(e.tooltip.options.timer, function(event) {
+              $(this).html('Prochain reset :<br />'+event.strftime('%H:%M:%S'));
+            });
+          });
+        }
+
+        if(typeof(countdown) !== 'undefined') {
+
+          marker.bindTooltip('', {countdownid: g.id+m.id, countdown: countdown, permanent: true, className: 'countdown', offset: [0, 10], direction: 'bottom'}).openTooltip();
+
+          var i = searchId(g.id+m.id, userCountdowns);
+          if(i >= 0) {
+            marker._tooltip.setContent('<div id="countdown-'+g.id+m.id+'">Prochain reset :</div>');
+            marker.on('tooltipopen', function(e) {
+              $($('#countdown-'+g.id+m.id)).countdown(userCountdowns[i]['date'], function(event) {
+                if(event.type === 'finish') {
+                  resetCountdown(g.id+m.id);
+                  $(this).html('');
+                } else {
+                  var totalDays = event.offset.weeks * 7 + event.offset.days;
+                  if(totalDays > 0) {
+                    $(this).html('Prochain reset :<br />'+event.strftime('%-D j. %H:%M:%S'));
+                  } else {
+                    $(this).html('Prochain reset :<br />'+event.strftime('%H:%M:%S'));
+                  }
+
+                }
+              });
+            });
+          }
+        }
+
+        if(checkbox)
+          marker.on('click', updateCurrentMarker);
+
+        if(typeof(m.polygon) !== 'undefined') {
+          var polygon = L.polygon(m.polygon, {color: color, fillColor: color}).addTo(g.group);
+        }
+
+
+
+        marker.addTo(g.group);
+        total++;
+
+        if(userMarkers.indexOf(g.id+m.id) >= 0 && !finished)
+          marker.setOpacity(.5);
+
+        if(params['debug'] && g.id !== 'region')
+          debugMarkers.push({name: g.id+m.id, marker: marker, coords: m.coords, icon: icon});
+
+        allMarkers[g.id+m.id] = marker;
+
+      });
+
+    });
+
+    $('#total').text(total);
+
+  }
+
+
+
+
+  // Limites de la carte
+  map.setMaxBounds(new L.LatLngBounds(unproject([1024,1024]), unproject([7168, 7168])));
+
+
+
+  // Afficher les coordonnées du clic
+  map.on('click', onMapClick);
+
+
+
+  // Masquer tous les layers
+  groups.forEach(function(e) {
+    map.removeLayer(window[e+'Group']);
+  });
+
+
+  // Debug
+  if(params['debug']) {
+    initMarkers();
+      $('#menu ul').html('');
+      debugMarkers.forEach(function(m, k) {
+          $('#menu ul').append('<li class="marker col-span-2"><a href="#" data-debug-marker="'+m.name+'" class="active"><img src="'+m.icon.options.iconUrl+'" /> <span>'+m.name+'</span></a></li>');
+      });
+
+      $('#menu ul li a').on('click', function(e) {
+          e.preventDefault();
+          var marker = $(this).data('debug-marker');
+
+          //jsObjects.find(x => x.b === 6)
+          debugMarkers.find(function(m) {
+              if(m.name === marker) {
+                  m.marker.openPopup();
+                  map.setView(unproject([m.coords[0], m.coords[1]]), 5);
+                  console.log(m);
+              }
+          });
+      });
+
+      groups.forEach(function(e) {
+          map.addLayer(window[e+'Group']);
+      });
+  }
+
+
+
+  // Gérer le contenu de la popup
+  map.on('popupopen', popUpOpen);
+
+
+
+  $(document).ready(function() {
+
+    // var updateDiscord = localStorage.getItem('update-discord');
+    // if(!updateDiscord) {
+    //   var lightbox = lity('#update-discord');
+    //   localStorage.setItem('update-discord', '1');
+    // }
+
+
+    $.get('api/user', function(res) {
+
+      if(typeof res.users !== 'undefined')
+        $('#total-users').text(res.users);
+
+      if(typeof res.visits !== 'undefined')
+        $('#total-visits').text(res.visits);
+
+      if(typeof res.login !== 'undefined') {
+        $('#discord').attr('href', res.login).attr('target', (window.location !== window.parent.location) ? '_blank' : '_self');
+      }
+
+      initMarkers();
+
+    });
+
+    var w = window.innerWidth;
+    if(w <= 500) {
+      $('body').removeClass('show-menu');
+      map.invalidateSize();
+    }
+
+    var zoom = (params.z && ['3', '4', '5'].indexOf(params.z) >= 0) ? params.z : 4;
+
+    if(params.x && params.y) {
+      map.setView(unproject([params.x, params.y]), zoom);
+    } else if(params.z) {
+      map.setZoom(zoom);
+    }
+
+    if(params.markers) {
+      var pmarkers = params.markers.split(',');
+      pmarkers.forEach(function(e) {
+        if(typeof window[e+'Group'] !== 'undefined') {
+          $('#menu a[data-type="'+e+'"]').addClass('active');
+          map.addLayer(window[e+'Group']);
+        }
+      });
+    }
+
+    if(params['hide-menu']) {
+      $('body').removeClass('show-menu');
+      map.invalidateSize();
+    }
+
+    $(document).on('change', 'input[type="checkbox"]', function() {
+      if(userLocal) {
+        saveUserMarkers($(this).data('id'), $(this).is(':checked'));
+      } else {
+        saveUserMarker($(this).data('id'), $(this).is(':checked'));
+      }
+
+    });
+
+    $('#menu a[data-type]').on('click', function(e){
+      e.preventDefault();
+
+      var type = $(this).data('type');
+
+      if($(this).hasClass('active')) {
+        map.removeLayer(window[type+'Group']);
+        if(!userLocal)
+          $.post('api/removemenu/'+type);
+      } else {
+        map.addLayer(window[type+'Group']);
+        if(!userLocal)
+          $.post('api/addmenu/'+type);
+      }
+
+      $(this).toggleClass('active');
+    });
+
+  });
+
+
+
