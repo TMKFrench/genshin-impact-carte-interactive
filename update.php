8f7e48a055a7044c5e0c8b959f77a37cbb776b82
Anthony Destenay <anthony.destenay@gmail.com>
2021-10-13 09:03:49 +0200

    Mise à jour de la carte pour la 2.2

--- update.php
+++ update.php
@@ -1,11 +1,11 @@
 <?php require 'includes/_fn.php';
 
-$operator = (isset($_GET['op']) && !empty($_GET['op'])) ? $_GET['op'] : null;
-$quantity = (isset($_GET['qt']) && !empty($_GET['qt'])) ? $_GET['qt'] : null;
-
-if(!$operator || !$quantity) {
-    return;
-}
+//$operator = (isset($_GET['op']) && !empty($_GET['op'])) ? $_GET['op'] : null;
+//$quantity = (isset($_GET['qt']) && !empty($_GET['qt'])) ? $_GET['qt'] : null;
+//
+//if(!$operator || !$quantity) {
+//    return;
+//}
 
 $db = new SQLite3Database('markers.db');
 $markers = $db->get_rows('SELECT id, x, y, uid, mgroup FROM genshin_map_marker');
@@ -13,13 +13,10 @@ $markers = $db->get_rows('SELECT id, x, y, uid, mgroup FROM genshin_map_marker')
 $updates = "## ---------- Updates ---------- ##\n\n";
 
 foreach($markers as $m) {
-    if($operator == 'plus') {
-        $x = $m->x + $quantity;
-        $y = $m->y + $quantity;
-    } elseif($operator == 'minus') {
-        $x = $m->x - $quantity;
-        $y = $m->y - $quantity;
-    }
+    // Pour la maj 2.2
+    $x = $m->x + 0;
+    $y = $m->y - 512;
+
     $updates .= "UPDATE genshin_map_marker SET x = '{$x}', y = '{$y}' WHERE id = '{$m->id}';\n";
 }
 
